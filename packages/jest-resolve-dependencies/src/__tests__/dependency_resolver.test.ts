99a84af94861df757eaf7c8424b64030bb0d4f28
lewibs <benjaminsl2000@gmail.com>
2025-02-07 13:28:34 -0500

    revert

--- packages/jest-resolve-dependencies/src/__tests__/dependency_resolver.test.ts
+++ packages/jest-resolve-dependencies/src/__tests__/dependency_resolver.test.ts
@@ -170,7 +170,7 @@ test('resolves dependencies correctly when maxDepth is set', () => {
   expect(filename).toBe("fileA")
 
   const paths = new Set([path.resolve(__dirname, '__fixtures__/fileA.js')]);
-  const resolved = dependencyResolver.resolveInverse(paths, filter);
+  const resolved = dependencyResolver.resolveInverse(paths, ()=>true);
   expect(resolved).toEqual([
     expect.stringContaining(
       path.join('__tests__', '__fixtures__', 'fileA.test.js'),
