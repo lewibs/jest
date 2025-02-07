4e324df7b8ae155b1a3b4cbb4a9f8f6d83271572
lewibs <benjaminsl2000@gmail.com>
2025-02-07 14:58:14 -0500

    update

--- packages/jest-resolve-dependencies/src/__tests__/dependency_resolver.test.ts
+++ packages/jest-resolve-dependencies/src/__tests__/dependency_resolver.test.ts
@@ -47,123 +47,123 @@ beforeEach(async () => {
   );
 });
 
-// test('resolves no dependencies for non-existent path', () => {
-//   const resolved = dependencyResolver.resolve('/non/existent/path');
-//   expect(resolved).toHaveLength(0);
-// });
+test('resolves no dependencies for non-existent path', () => {
+  const resolved = dependencyResolver.resolve('/non/existent/path');
+  expect(resolved).toHaveLength(0);
+});
 
-// test('resolves dependencies for existing path', () => {
-//   const resolved = dependencyResolver.resolve(
-//     path.resolve(__dirname, '__fixtures__', 'file.js'),
-//   );
-//   expect(resolved).toEqual([
-//     expect.stringContaining('jest-resolve-dependencies'),
-//     expect.stringContaining('jest-regex-util'),
-//   ]);
-// });
+test('resolves dependencies for existing path', () => {
+  const resolved = dependencyResolver.resolve(
+    path.resolve(__dirname, '__fixtures__', 'file.js'),
+  );
+  expect(resolved).toEqual([
+    expect.stringContaining('jest-resolve-dependencies'),
+    expect.stringContaining('jest-regex-util'),
+  ]);
+});
 
-// test('includes the mocks of dependencies as dependencies', () => {
-//   const resolved = dependencyResolver.resolve(
-//     path.resolve(__dirname, '__fixtures__/hasMocked/file.test.js'),
-//   );
+test('includes the mocks of dependencies as dependencies', () => {
+  const resolved = dependencyResolver.resolve(
+    path.resolve(__dirname, '__fixtures__/hasMocked/file.test.js'),
+  );
 
-//   expect(resolved).toEqual([
-//     expect.stringContaining(path.join('hasMocked', 'file.js')),
-//     expect.stringContaining(path.join('hasMocked', '__mocks__', 'file.js')),
-//     expect.stringContaining(path.join('__mocks__', 'fake-node-module.js')),
-//   ]);
-// });
+  expect(resolved).toEqual([
+    expect.stringContaining(path.join('hasMocked', 'file.js')),
+    expect.stringContaining(path.join('hasMocked', '__mocks__', 'file.js')),
+    expect.stringContaining(path.join('__mocks__', 'fake-node-module.js')),
+  ]);
+});
 
-// test('resolves dependencies for scoped packages', () => {
-//   const resolved = dependencyResolver.resolve(
-//     path.resolve(__dirname, '__fixtures__', 'scoped.js'),
-//   );
-//   expect(resolved).toEqual([
-//     expect.stringContaining(path.join('@myorg', 'pkg')),
-//   ]);
-// });
+test('resolves dependencies for scoped packages', () => {
+  const resolved = dependencyResolver.resolve(
+    path.resolve(__dirname, '__fixtures__', 'scoped.js'),
+  );
+  expect(resolved).toEqual([
+    expect.stringContaining(path.join('@myorg', 'pkg')),
+  ]);
+});
 
-// test('resolves no inverse dependencies for empty paths set', () => {
-//   const paths = new Set<string>();
-//   const resolved = dependencyResolver.resolveInverse(paths, filter);
-//   expect(resolved).toHaveLength(0);
-// });
+test('resolves no inverse dependencies for empty paths set', () => {
+  const paths = new Set<string>();
+  const resolved = dependencyResolver.resolveInverse(paths, filter);
+  expect(resolved).toHaveLength(0);
+});
 
-// test('resolves no inverse dependencies for set of non-existent paths', () => {
-//   const paths = new Set(['/non/existent/path', '/another/one']);
-//   const resolved = dependencyResolver.resolveInverse(paths, filter);
-//   expect(resolved).toHaveLength(0);
-// });
+test('resolves no inverse dependencies for set of non-existent paths', () => {
+  const paths = new Set(['/non/existent/path', '/another/one']);
+  const resolved = dependencyResolver.resolveInverse(paths, filter);
+  expect(resolved).toHaveLength(0);
+});
 
-// test('resolves inverse dependencies for existing path', () => {
-//   const paths = new Set([path.resolve(__dirname, '__fixtures__/file.js')]);
-//   const resolved = dependencyResolver.resolveInverse(paths, filter);
-//   expect(resolved).toEqual([
-//     expect.stringContaining(
-//       path.join('__tests__', '__fixtures__', 'file.test.js'),
-//     ),
-//   ]);
-// });
+test('resolves inverse dependencies for existing path', () => {
+  const paths = new Set([path.resolve(__dirname, '__fixtures__/file.js')]);
+  const resolved = dependencyResolver.resolveInverse(paths, filter);
+  expect(resolved).toEqual([
+    expect.stringContaining(
+      path.join('__tests__', '__fixtures__', 'file.test.js'),
+    ),
+  ]);
+});
 
-// test('resolves inverse dependencies of mock', () => {
-//   const paths = new Set([
-//     path.resolve(__dirname, '__fixtures__/hasMocked/__mocks__/file.js'),
-//   ]);
-//   const resolved = dependencyResolver.resolveInverse(paths, filter);
+test('resolves inverse dependencies of mock', () => {
+  const paths = new Set([
+    path.resolve(__dirname, '__fixtures__/hasMocked/__mocks__/file.js'),
+  ]);
+  const resolved = dependencyResolver.resolveInverse(paths, filter);
 
-//   expect(resolved).toEqual([
-//     expect.stringContaining(
-//       path.join('__tests__/__fixtures__/hasMocked/file.test.js'),
-//     ),
-//   ]);
-// });
+  expect(resolved).toEqual([
+    expect.stringContaining(
+      path.join('__tests__/__fixtures__/hasMocked/file.test.js'),
+    ),
+  ]);
+});
 
-// test('resolves inverse dependencies from available snapshot', () => {
-//   const paths = new Set([
-//     path.resolve(__dirname, '__fixtures__/file.js'),
-//     path.resolve(__dirname, '__fixtures__/__snapshots__/related.test.js.snap'),
-//   ]);
-//   const resolved = dependencyResolver.resolveInverse(paths, filter);
-//   expect(resolved).toEqual(
-//     expect.arrayContaining([
-//       expect.stringContaining(
-//         path.join('__tests__', '__fixtures__', 'file.test.js'),
-//       ),
-//       expect.stringContaining(
-//         path.join('__tests__', '__fixtures__', 'related.test.js'),
-//       ),
-//     ]),
-//   );
-// });
+test('resolves inverse dependencies from available snapshot', () => {
+  const paths = new Set([
+    path.resolve(__dirname, '__fixtures__/file.js'),
+    path.resolve(__dirname, '__fixtures__/__snapshots__/related.test.js.snap'),
+  ]);
+  const resolved = dependencyResolver.resolveInverse(paths, filter);
+  expect(resolved).toEqual(
+    expect.arrayContaining([
+      expect.stringContaining(
+        path.join('__tests__', '__fixtures__', 'file.test.js'),
+      ),
+      expect.stringContaining(
+        path.join('__tests__', '__fixtures__', 'related.test.js'),
+      ),
+    ]),
+  );
+});
 
-// test('resolves dependencies correctly when dependency resolution fails', () => {
-//   jest.spyOn(runtimeContextResolver, 'resolveModule').mockImplementation(() => {
-//     throw new Error('resolveModule has failed');
-//   });
-//   jest.spyOn(runtimeContextResolver, 'getMockModule').mockImplementation(() => {
-//     throw new Error('getMockModule has failed');
-//   });
+test('resolves dependencies correctly when dependency resolution fails', () => {
+  jest.spyOn(runtimeContextResolver, 'resolveModule').mockImplementation(() => {
+    throw new Error('resolveModule has failed');
+  });
+  jest.spyOn(runtimeContextResolver, 'getMockModule').mockImplementation(() => {
+    throw new Error('getMockModule has failed');
+  });
 
-//   const resolved = dependencyResolver.resolve(
-//     path.resolve(__dirname, '__fixtures__', 'file.test.js'),
-//   );
+  const resolved = dependencyResolver.resolve(
+    path.resolve(__dirname, '__fixtures__', 'file.test.js'),
+  );
 
-//   expect(resolved).toEqual([]);
-// });
+  expect(resolved).toEqual([]);
+});
 
-// test('resolves dependencies correctly when mock dependency resolution fails', () => {
-//   jest.spyOn(runtimeContextResolver, 'getMockModule').mockImplementation(() => {
-//     throw new Error('getMockModule has failed');
-//   });
+test('resolves dependencies correctly when mock dependency resolution fails', () => {
+  jest.spyOn(runtimeContextResolver, 'getMockModule').mockImplementation(() => {
+    throw new Error('getMockModule has failed');
+  });
 
-//   const resolved = dependencyResolver.resolve(
-//     path.resolve(__dirname, '__fixtures__', 'file.test.js'),
-//   );
+  const resolved = dependencyResolver.resolve(
+    path.resolve(__dirname, '__fixtures__', 'file.test.js'),
+  );
 
-//   expect(resolved).toEqual([
-//     expect.stringContaining(path.join('__tests__', '__fixtures__', 'file.js')),
-//   ]);
-// });
+  expect(resolved).toEqual([
+    expect.stringContaining(path.join('__tests__', '__fixtures__', 'file.js')),
+  ]);
+});
 
 test('resolves dependencies correctly when maxDepth is set', () => {
   const filename = require("./__fixtures__/fileD.js")
